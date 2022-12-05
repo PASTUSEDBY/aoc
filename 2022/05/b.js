7ad750967d80ddeee400fa8f90b3d72bf2213e28
PASTUSEDBY <samantaalok401@gmail.com>
2022-12-05 19:17:43 +0530

    Day 5

---
+++ 2022/05/b.js
@@ -0,0 +1,31 @@
+const { parse } = require('./parse.js');
+
+const readline = require('readline').createInterface({
+  input : process.stdin
+});
+
+let inputs = [];
+let parseInputs = null;
+
+readline.on('line', line => {
+  if (!line) {
+    parseInputs = parse(inputs);
+    return;
+  }
+
+  if (!parseInputs) {
+    inputs.push(line);
+    return;
+  }
+
+  let [, count, start, end ] = line.match(/move (\d+) from (\d+) to (\d+)/).map(Number);
+
+  let length = parseInputs[start - 1].length;
+  parseInputs[end - 1].push(
+    ...parseInputs[start - 1].splice(length - count)
+  );
+});
+
+readline.on('close', () => {
+  console.log(parseInputs.map(k => k[k.length - 1].replace(/[\[\]]/g, '')).join(""));
+});
